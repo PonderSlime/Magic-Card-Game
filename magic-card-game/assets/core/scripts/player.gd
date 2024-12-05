f62f51da82c3086889323a5a5fd0b86ee2536077
PonderSlime <micah.j.edwards1@gmail.com>
2024-12-04 17:31:51 -0700

    added new sprites, removed random files

--- magic-card-game/assets/core/scripts/player.gd
+++ magic-card-game/assets/core/scripts/player.gd
@@ -1,6 +1,7 @@
 extends Node
 
-var player_health = 21
+var initial_health: float = 21
+var player_health: float = initial_health
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
